96f51de006701ed9dc3ed943822fd7b87472171e
YordTanin <6110110193@psu.ac.th>
2020-08-30 16:13:09 +0700

    section 6

---
+++ screen/WeatherScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native';
+
+export default function WeatherScreen({route}) {
+    return (
+        <View>
+            <Weather zipCode={route.params.zipCode} />
+        </View>
+        );
+}
